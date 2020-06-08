0dcf716d5869edd4baa59152b03da8cf186d8d25
benjamintemitope <benjaminfranklin085@gmail.com>
2020-06-08 22:20:32 +0100

    Initial commit

---
+++ js/intel.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".form-control").focus(function(){
+        $(this).parent(".input-group").addClass("input-group-focus")
+    });
+    $(".form-control").blur(function(){
+        $(this).parent(".input-group").removeClass("input-group-focus")
+    });
+
+
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+	(function() {
+	  'use strict';
+	  window.addEventListener('load', function() {
+	    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+	    var forms = document.getElementsByClassName('needs-validation');
+	    // Loop over them and prevent submission
+	    var validation = Array.prototype.filter.call(forms, function(form) {
+	      form.addEventListener('submit', function(event) {
+	        if (form.checkValidity() === false) {
+	          event.preventDefault();
+	          event.stopPropagation();
+	        }
+	        form.classList.add('was-validated');
+	      }, false);
+	    });
+	  }, false);
+	})();
+});
